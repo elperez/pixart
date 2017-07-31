94d51ac0f8189a15cd4fe177aa0d459e7e061b32
kit.perez <kit.perez@gmail.com>
2017-07-31 16:18:17 +1000

    commit 4: change color to green

--- pixart.js
+++ pixart.js
@@ -3,7 +3,7 @@ var $brush = $(".brush");
 
 document.getElementById("set-color").addEventListener("click", function(event){
     event.preventDefault()
-    changeColor();
+    changeColor($colorInput.val(), $brush);
 });
 
 document.getElementById("set-color").addEventListener('keypress', function(e){
@@ -11,18 +11,24 @@ document.getElementById("set-color").addEventListener('keypress', function(e){
 
   var key = e.which || e.keyCode;
   if (key === 13) { // 13 is enter
-    changeColor();
+    changeColor($colorInput.val(), $brush);
   }
 });
 
-function changeColor(){
-  colorInput = $colorInput.val();
-  $brush.css('background-color', colorInput);
+function changeColor(color, element){
+  debugger;
+  element.css('background-color', color);
 }
 function addDivs(){
   for (var i = 0; i < 20; i++) {
     var $div = $("<div>");
-     $("body").append( $div );
+    $div.attr("class","brush");
+    $div.bind('click', function() {
+      debugger;
+
+      changeColor("green",$(this));
+    });
+    $("body").append( $div );
   }
 }
 
