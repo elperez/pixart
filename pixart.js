4a8c17482487aea8f179fd8ee0bee1b5f4ef5762
kit.perez <kit.perez@gmail.com>
2017-07-31 15:50:51 +1000

    commit 2: added change color on enter key press

--- pixart.js
+++ pixart.js
@@ -6,5 +6,14 @@ document.getElementById("set-color").addEventListener("click", function(event){
     event.preventDefault()
     colorInput = $colorInput.val();
     $brush.css('background-color', colorInput);
-    console.log($colorInput);
+});
+
+document.getElementById("set-color")..addEventListener('keypress', function(e){
+  console.log(inputBox.value);
+
+  var key = e.which || e.keyCode;
+  if (key === 13) { // 13 is enter
+    colorInput = $colorInput.val();
+    $brush.css('background-color', colorInput);
+  }
 });
