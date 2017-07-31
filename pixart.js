dd307e0061a598e5531e7774592355978aab9eca
kit.perez <kit.perez@gmail.com>
2017-07-31 16:02:42 +1000

    commit 3: add 20 divs

--- pixart.js
+++ pixart.js
@@ -1,19 +1,29 @@
 var $colorInput = $("#color-field");
 var $brush = $(".brush");
 
-
 document.getElementById("set-color").addEventListener("click", function(event){
     event.preventDefault()
-    colorInput = $colorInput.val();
-    $brush.css('background-color', colorInput);
+    changeColor();
 });
 
-document.getElementById("set-color")..addEventListener('keypress', function(e){
+document.getElementById("set-color").addEventListener('keypress', function(e){
   console.log(inputBox.value);
 
   var key = e.which || e.keyCode;
   if (key === 13) { // 13 is enter
-    colorInput = $colorInput.val();
-    $brush.css('background-color', colorInput);
+    changeColor();
   }
 });
+
+function changeColor(){
+  colorInput = $colorInput.val();
+  $brush.css('background-color', colorInput);
+}
+function addDivs(){
+  for (var i = 0; i < 20; i++) {
+    var $div = $("<div>");
+     $("body").append( $div );
+  }
+}
+
+addDivs();
